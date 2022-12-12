eeb1dc64053bb9268519ec39aac31a9a28e214a5
Karam Issa <karamissa79@gmail.com>
2022-12-12 15:40:00 +0300

    Clean up firebase config file

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import {
-  GoogleAuthProvider,
-  getAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  signInWithPopup,
-  sendPasswordResetEmail,
-  signOut
-} from 'firebase/auth';
-import {
-  getFirestore,
-  collection,
-  query,
-  where,
-  getDocs,
-  addDoc
-} from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -29,74 +13,3 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
-
-// Custom functions to manage auth
-const googleProvider = new GoogleAuthProvider();
-const logInWithGoogle = async () => {
-  try {
-    const res = await signInWithPopup(auth, googleProvider);
-
-    const user = res.user;
-    const q = query(collection(db, 'users'), where('uid', '==', user.uid));
-
-    const docs = await getDocs(q);
-    if (docs.docs.length === 0) {
-      await addDoc(collection(db, 'users'), {
-        uid: user.uid,
-        name: user.displayName,
-        authProvider: 'google',
-        email: user.email
-      });
-    }
-  } catch (err) {
-    console.error(err);
-  }
-};
-
-const registerWithEmailAndPassword = async (name, email, password) => {
-  try {
-    const res = await createUserWithEmailAndPassword(auth, email, password);
-
-    const user = res.user;
-
-    await addDoc(collection(db, 'users'), {
-      uid: user.uid,
-      name,
-      authProvider: 'local',
-      email
-    });
-  } catch (err) {
-    console.error(err);
-  }
-};
-
-const logInWithEmailAndPassword = async (email, password) => {
-  try {
-    await signInWithEmailAndPassword(auth, email, password);
-  } catch (err) {
-    console.error(err);
-    alert(err.message);
-  }
-};
-
-const sendPasswordReset = async (email) => {
-  try {
-    await sendPasswordResetEmail(auth, email);
-  } catch (err) {
-    console.error(err);
-  }
-};
-
-const logOut = () => {
-  signOut(auth);
-};
-
-export {
-  auth,
-  db,
-  logInWithGoogle,
-  registerWithEmailAndPassword,
-  logInWithEmailAndPassword,
-  sendPasswordReset,
-  logOut
-};
