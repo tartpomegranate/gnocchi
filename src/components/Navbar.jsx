596f4a439c46cfb77c8e9cab3719966b77563bdf
Karam Issa <karamissa79@gmail.com>
2022-12-14 20:16:27 +0300

    Remove SignUp & SignIn pages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,20 +6,12 @@ const Navbar = ({ isDarkMode, toggleDarkMode }) => {
     <nav className="p-6 flex justify-between items-center dark:bg-gray-800 dark:text-white">
       <h1 className="text-3xl font-bold">Gnocchi</h1>
 
-      <div className="flex justify-between items-center gap-4">
-        <Moon
-          size={32}
-          weight={isDarkMode ? 'fill' : 'regular'}
-          onClick={toggleDarkMode}
-          className="p-1 cursor-pointer rounded-full dark:hover:bg-gray-600 hover:bg-gray-300  transition duration-200"
-        />
-        <Link
-          to="/signin"
-          className="bg-sky-500 text-white py-2 px-4 rounded-md font-semibold hover:bg-sky-600 transition duration-200"
-        >
-          Sign in
-        </Link>
-      </div>
+      <Moon
+        size={32}
+        weight={isDarkMode ? 'fill' : 'regular'}
+        onClick={toggleDarkMode}
+        className="p-1 cursor-pointer rounded-full dark:hover:bg-gray-600 hover:bg-gray-300  transition duration-200"
+      />
     </nav>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -5,29 +5,11 @@ import './index.css';
 
 import Root from './routes/Root';
 import ErrorPage from './routes/ErrorPage';
-import SignIn from './routes/SignIn';
-import SignUp from './routes/SignUp';
-import PasswordReset from './routes/PasswordReset';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
-    errorElement: <ErrorPage />
-  },
-  {
-    path: '/signin',
-    element: <SignIn />,
-    errorElement: <ErrorPage />
-  },
-  {
-    path: '/signup',
-    element: <SignUp />,
-    errorElement: <ErrorPage />
-  },
-  {
-    path: '/password-reset',
-    element: <PasswordReset />,
     errorElement: <ErrorPage />
   }
 ]);
